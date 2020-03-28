6b8b4539334383b72a4db300e8a18bf4476df929
drkosti <62425902+drkosti@users.noreply.github.com>
2020-03-28 19:43:21 +0100

    Revert "Update ustawienia.php"
    This reverts commit a8147ee3496cb23bdc0b6e2706e7e3d4df4b7449.

--- radio-kosti/inc/ustawienia.php
+++ radio-kosti/inc/ustawienia.php
@@ -4,6 +4,6 @@ $slogan='Najlepsze radio w internetach - szablon radiowy by kosti';
 $panel='kosti';
 $ip='s1.slotex.pl';
 $port='7458';
-$strona='https://localhost/radio-kosti/';
+$strona='https://kosti24.000webhostapp.com/radio-kosti/';
 $fb='https://facebook.com/ekostipl';
 ?>
