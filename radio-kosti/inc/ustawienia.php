fa3fde679f05a046c5f73f558f3e3cfc8cce10f4
drkosti <62425902+drkosti@users.noreply.github.com>
2020-03-30 22:08:14 +0200

    Delete ustawienia.php

--- radio-kosti/inc/ustawienia.php
+++
@@ -1,9 +0,0 @@
-﻿<?PHP
-$nazwa='Kosti radio';
-$slogan='Najlepsze radio w internetach - szablon radiowy by kosti';
-$panel='kosti';
-$ip='s1.slotex.pl';
-$port='7458';
-$strona='https://kosti24.000webhostapp.com/radio-kosti/';
-$fb='https://facebook.com/ekostipl';
-?>
